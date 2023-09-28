f074f2ab2ac1624bb5177423a13a6712cd7406e9
Adrian reyes <adriandmc05@gmail.com>
2023-09-28 13:03:48 -0400

    add handle action on modal component

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -11,11 +11,11 @@ export class ModalComponent {
   @Input() type: string = 'alert';
   @Input() showAction: boolean = false;
   @Input() showModal: boolean = false;
+  @Input() productId: string = 'false';
+  @Input() loading: boolean = false;
   @Output() closeModal = new EventEmitter<boolean>();
+  @Output() handleAction = new EventEmitter<string>();
+  @Output() handleLoading = new EventEmitter<boolean>();
 
   constructor() {}
-
-  close() {
-    this.closeModal.emit(false);
-  }
 }
